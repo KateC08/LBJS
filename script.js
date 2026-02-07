829dd738bd799f5feb08a62ead8d1ba29660eaae
KateC08 <coming.katelyn08@gmail.com>
2026-02-07 13:17:10 +0800

    Added the logic of the input field to make the next decision

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ let name = "katelyn"
 let num = 21
 let isLoggedIn = true
 
-
-let user = document.getElemendById("username").value;
+let user = document.getElementById("username").value;
 let pass = document.getElementById("password").value;
 
 function check() {
 
+    if(user !== "" & pass !== "") {
+        alert("Success, " + user)
+    } else {
+        alert("Failed!")
+    }
 }
