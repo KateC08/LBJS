fabf58970d7b52d0eb3509d46cf141171db74438
KateC08 <coming.katelyn08@gmail.com>
2026-02-07 15:11:37 +0800

    Added another function for checking user role by storing data in an array

--- script.js
+++ script.js
@@ -3,6 +3,25 @@ let num = 21
 let isLoggedIn = true
 
 
+function checks() {
+    let actors = ["admin", "guest"]
+    let diffUsers = ["kate", "katya"]
+    let diffPass = ["qwerty12", "huhuhu11"]
+
+    let user = document.getElementById("username").value;
+    let pass = document.getElementById("password").value;
+    let found = false;
+
+    for(let i = 0; i <= diffUsers.length; i++) {
+        if(user == diffUsers[i] && pass == diffPass[i]) {
+            alert("Welcome, " + actors[i])    
+            return;      
+        } 
+    }
+    alert("Failed")
+
+}
+
 function check() {
     
     let user = document.getElementById("username").value;
